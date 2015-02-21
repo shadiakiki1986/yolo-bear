634654d09a8d3271d7b355419130db997e8a5d96
Shadi Akiki <shadiakiki1986@gmail.com>
2015-02-21 18:55:29 +0200

    improved build script

--- crosswalk_build.sh
+++ crosswalk_build.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-# Prerequisite https://crosswalk-project.org/documentation/downloads.html
+# Prerequisite, crosswalk_install.sh
+# https://crosswalk-project.org/documentation/downloads.html
 
 set -e
 
 cd ~/crosswalk-10.39.235.15
-python make_apk.py --package=com.akikieng.genesis.yolobear --manifest=/home/ubuntu/yolo-bear/manifest.json
-
-xwalkApk="/home/ubuntu/crosswalk-10.39.235.15/Yolobear_0.0.0.1_arm.apk"
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/yolo-bear.keystore $xwalkApk yolo-bear
-jarsigner -verify -verbose -certs $xwalkApk
-rm -i ~/yolo-bear-release-signed.apk
-$ANDROID_HOME/build-tools/21.1.2/zipalign -v 4 $xwalkApk ~/yolo-bear-release-signed.apk
-
+python make_apk.py \
+  --package=com.akikieng.genesis.yolobear \
+  --manifest=/home/ubuntu/yolo-bear/manifest.json \
+  --keystore-path=~/yolo-bear.keystore \
+  --keystore-alias=yolo-bear \
+  --app-version=0.0.0 \
+  --arch=arm \
+  --target-dir=~
+$ANDROID_HOME/build-tools/21.1.2/zipalign -c -v 4 ~/Yolobear_0.0.0.1_arm.apk|tail -n 1
 
 echo "Completed build app for android"
 echo "Version: "
-$ANDROID_HOME/build-tools/21.1.2/aapt dump badging ~/yolo-bear-release-signed.apk |head -n 1
-echo "App is now available in ~/yolo-bear-release-signed.apk"
+$ANDROID_HOME/build-tools/21.1.2/aapt dump badging ~/Yolobear_0.0.0.1_arm.apk |head -n 1
+echo "App is now available in ~/Yolobear_0.0.0.1_arm.apk"
 echo "and you could upload it to Google play at https://play.google.com/apps/publish/"
 
+cd -
