a2cefde0e33939bffc8d41d267b09a5ffbd3db8a
Shadi Akiki <shadiakiki1986@gmail.com>
2015-02-21 18:34:51 +0200

    added keygen to install

--- crosswalk_install.sh
+++ crosswalk_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Prerequisites: java -v, ant -v, python --version
 
+cd ~/Downloads
 wget https://download.01.org/crosswalk/releases/crosswalk/android/stable/10.39.235.15/crosswalk-10.39.235.15.zip
-cd crosswalk-10.39.235.15
+
+# generate keystore for signing app before upload to google play
+keytool -v -genkey -v -keystore yolo-bear.keystore -alias yolo-bear -keyalg RSA -validity 10000
