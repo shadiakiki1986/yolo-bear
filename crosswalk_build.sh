665732666d98cc2faf0321ce5ee9744338ae1a58
Shadi Akiki <shadiakiki1986@gmail.com>
2015-02-21 18:30:32 +0200

    improved build script to include signing

--- crosswalk_build.sh
+++ crosswalk_build.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 # Prerequisite https://crosswalk-project.org/documentation/downloads.html
 
+set -e
+
 cd ~/crosswalk-10.39.235.15
 python make_apk.py --package=com.akikieng.genesis.yolobear --manifest=/home/ubuntu/yolo-bear/manifest.json
+
+xwalkApk="/home/ubuntu/crosswalk-10.39.235.15/Yolobear_0.0.0.1_arm.apk"
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/yolo-bear.keystore $xwalkApk yolo-bear
+jarsigner -verify -verbose -certs $xwalkApk
+rm -i ~/yolo-bear-release-signed.apk
+$ANDROID_HOME/build-tools/21.1.2/zipalign -v 4 $xwalkApk ~/yolo-bear-release-signed.apk
+
+
+echo "Completed build app for android"
+echo "Version: "
+$ANDROID_HOME/build-tools/21.1.2/aapt dump badging ~/yolo-bear-release-signed.apk |head -n 1
+echo "App is now available in ~/yolo-bear-release-signed.apk"
+echo "and you could upload it to Google play at https://play.google.com/apps/publish/"
+
