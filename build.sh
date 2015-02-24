58f267b54f5b61412962fa5b93f86e43a4265137
Shadi Akiki <shadiakiki1986@gmail.com>
2015-02-24 08:48:24 +0200

    build.sh mistake in variable name including braces

--- build.sh
+++ build.sh
@@ -11,15 +11,15 @@ python make_apk.py \
   --manifest=/home/ubuntu/yolo-bear/manifest.json \
   --keystore-path=~/yolo-bear.keystore \
   --keystore-alias=yolo-bear \
-  --app-version={$version} \
+  --app-version=${version} \
   --arch=arm \
   --target-dir=~
-$ANDROID_HOME/build-tools/21.1.2/zipalign -c -v 4 ~/Yolobear_{$version}_arm.apk|tail -n 1
+$ANDROID_HOME/build-tools/21.1.2/zipalign -c -v 4 ~/Yolobear_${version}_arm.apk|tail -n 1
 
 echo "Completed build app for android"
 echo "Version: "
-$ANDROID_HOME/build-tools/21.1.2/aapt dump badging ~/Yolobear_{$version}_arm.apk |head -n 1
-echo "App is now available in ~/Yolobear_{$version}_arm.apk"
+$ANDROID_HOME/build-tools/21.1.2/aapt dump badging ~/Yolobear_${version}_arm.apk |head -n 1
+echo "App is now available in ~/Yolobear_${version}_arm.apk"
 echo "and you could upload it to Google play at https://play.google.com/apps/publish/"
 
 cd -
