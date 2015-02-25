b61bef2f4ea4b0e29ecbe9ec61073b0cbd59cdd5
Shadi Akiki <shadiakiki1986@gmail.com>
2015-02-25 15:53:38 +0200

    some renames that made the build fail

--- build.sh
+++ build.sh
@@ -5,21 +5,22 @@
 set -e
 
 version=0.0.0
-cd ~/crosswalk-10.39.235.15
-python make_apk.py \
+output="~/Yolobear_${version}_arm.apk"
+
+rm -f $output
+python ~/crosswalk-10.39.235.15/make_apk.py \
   --package=com.akikieng.genesis.yolobear \
   --manifest=/home/ubuntu/yolo-bear/manifest.json \
   --keystore-path=~/yolo-bear.keystore \
   --keystore-alias=yolo-bear \
   --app-version=${version} \
   --arch=arm \
-  --target-dir=~
-$ANDROID_HOME/build-tools/21.1.2/zipalign -c -v 4 ~/Yolobear_${version}_arm.apk|tail -n 1
+  --target-dir=~ \
+  --verbose
+$ANDROID_HOME/build-tools/21.1.2/zipalign -c -v 4 $output|tail -n 1
 
 echo "Completed build app for android"
 echo "Version: "
-$ANDROID_HOME/build-tools/21.1.2/aapt dump badging ~/Yolobear_${version}_arm.apk |head -n 1
-echo "App is now available in ~/Yolobear_${version}_arm.apk"
+$ANDROID_HOME/build-tools/21.1.2/aapt dump badging $output |head -n 1
+echo "App is now available in $output"
 echo "and you could upload it to Google play at https://play.google.com/apps/publish/"
-
-cd -
