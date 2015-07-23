7204605fe6307359173b0a42e637c9a8284d890f
Shadi Akiki <shadiakiki1986@gmail.com>
2015-07-23 05:02:10 +0000

    added config to ghpages branch

---
+++ js/config.js
@@ -0,0 +1,8 @@
+PEERJS_KEY='QMLEtmK'; // openssl rand -base64 18      
+PEERJS_HOST='genesis.akikieng.com';
+PEERJS_PORT=9000;
+PEERJS_PATH='/yolo-bear';  
+PEERJS_DEBUG=false;
+PEERJS_MAX_RECONN_ATTEMPT=3;
+PEERJS_TIMEOUT=30000;
+YOLOBEAR_SERVER_URL='http://genesis.akikieng.com/yolo-bear-server/api';
